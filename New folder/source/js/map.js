013059de923b4b82d1a6034c8983dc4c3d90e0b0
asvspb <asv.devpro@gmail.com>
2020-01-29 16:47:35 +0300

    Правки к защите

--- New folder/source/js/map.js
+++
@@ -1,5 +0,0 @@
-var live_map = document.getElementById("live_map");
-var map_image = document.querySelector(".page-footer__map-image");
-
-map_image.classList.add("hidden");
-live_map.classList.remove("hidden");
